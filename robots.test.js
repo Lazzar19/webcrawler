7d6c29c2e48f7b6ca673ca3523212386a8e17e6a
Lazzar19 <lazarnikolic863@gmail.com>
2025-06-23 11:06:13 +0200

    Tests for parseRobotsTxt function

--- robots.test.js
+++ robots.test.js
@@ -60,7 +60,7 @@ test('return empty string if status != 200 (403 example)', async () => {
 })
 
 test('malformed URL input', async () => {
-    const result = await handeler.fetchRobotsTxt('malformed-url');
+    const result = await handler.fetchRobotsTxt('malformed-url');
     expect(result).toBe("");
 })
 
@@ -73,12 +73,12 @@ test('empty string if there is some network error', async () => {
 
 test('return empty string if body of robots.txt is empty', async() => {
     fetch.mockResolvedValue( {status:200, text: async() => ''} );
-    const result = await handeler.fetchRobotsTxt('https://empty.com');
+    const result = await handler.fetchRobotsTxt('https://empty.com');
     expect(result).toBe("");
 })
 
 test('use correct protocol from URL (https)', async () => {
-    const mockText = 'User-Agent: *\nDisalled: /';
+    const mockText = 'User-Agent: *\nDisallow: /';
     fetch.mockResolvedValue({status: 200, text: async() => mockText});
 
     const result = await handler.fetchRobotsTxt('https://secure-site.com');
@@ -87,7 +87,7 @@ test('use correct protocol from URL (https)', async () => {
 
 
 test('use correct protocol from URL (http)', async () => {
-    const mockText = 'User-Agent: *\nDisalled: /';
+    const mockText = 'User-Agent: *\nDisallow: /';
     fetch.mockResolvedValue({status: 200, text: async() => mockText});
 
     const result = await handler.fetchRobotsTxt('https://non-secure-site.com');
@@ -102,7 +102,150 @@ test('no re-fetch if cache is still valid', async () => {
     })
 
     const result = await handler.fetchRobotsTxt('https://cachetest.com');
-    expect(result).not.toHaveBeenCalled();
+    expect(fetch).not.toHaveBeenCalled();
     expect(result).toBe(cachedContent);
 
+})
+
+
+test('successfull parsing simple user-agent block', () => {
+    let content = `
+        User-agent: *
+        Disallow: /admin
+        Allow: /pablic
+        Crawl-delay: 10
+    
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.has('*')).toBe(true);
+    const rules = parsed.agents.get('*');
+    expect(rules.disallow).toEqual(['/admin']);
+    expect(rules.allow).toEqual(['/pablic']);
+    expect(rules.crawlDelay).toBe(10);
+
+})
+
+test('successfull parsing more than 1 user-agent block', () => {
+    let content = `
+        User-agent: googlebot
+        Disallow: /private
+
+
+        User-agent: bingbot
+        Disallow: /secret
+        Crawl-delay: 5
+    `
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.has('googlebot')).toBe(true);
+    expect(parsed.agents.has('bingbot')).toBe(true);
+
+    expect(parsed.agents.get('googlebot').disallow).toEqual(['/private']);
+    expect(parsed.agents.get('bingbot').disallow).toEqual(['/secret']);
+    expect(parsed.agents.get('bingbot').crawlDelay).toBe(5);
+
+
+})
+
+test("sitemap parsing", () => {
+    let content = `
+        Sitemap: https://example.com/sitemap.xml
+        User-agent: *
+        Disallow: /tmp
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.sitemap).toEqual(['https://example.com/sitemap.xml']);
+    expect(parsed.agents.get('*').disallow).toEqual(['/tmp']);
+})
+
+
+test('malformed/unknown directives', () => {
+    const content = `
+        User-agent: *
+        Disallow: /admin
+        Allow: /public
+        Foo: invalid
+        Crawl-delay: 5
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+
+    expect(parsed.agents.has('*')).toBe(true);
+    expect(parsed.agents.get('*').disallow).toEqual(['/admin']);
+    expect(parsed.agents.get('*').allow).toEqual(['/public']);
+    expect(parsed.agents.get('*').crawlDelay).toBe(5);
+})
+
+
+test('ignores comments and empty lines in file', () => {
+    const content = `
+        # some text
+        User-agent: *
+
+        Disallow: /admin
+        # other text
+        Allow: /public
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get('*')).toBe(true);
+
+})
+
+test('multiple agents in one block', () => {
+    const content = `
+        User-agent: googlebot
+        User-agent: bingbot
+        Disallow: /admin
+    
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get('googlebot').disallow).toEqual(['/admin']);
+    expect(parsed.agents.get('bingbot').disallow).toBe(['/admin']);
+
+})
+
+test('fallback to wildcard rules if agents can not be found', () => {
+    const content = `
+        User-agent: *
+        Disallow: /block
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get('*')).toBe(true);
+})
+
+
+test('crawlDelay is decimal number', () => {
+    const content = `
+        User-agent: *
+        Disallow: /block
+        Crawl-delay: 2.2
+    `;
+
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get('*').crawlDelay).toBeCloseTo(2.2);
+
+})
+
+test('disallow field being empty', () => {
+    const content = `
+        User-agent: *
+        Disallow: 
+    `;
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get("*").disallow).toEqual(['']);
+})
+
+
+test('directive with extra colons', () => {
+    const content = `
+        User-agent: *
+        Disallow: /path:with:colon
+    `;
+    const parsed = handler.parseRobotsTxt(content);
+    expect(parsed.agents.get('*').disallow).toEqual(['/path:with:colon']);
 })
